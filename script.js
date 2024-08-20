889f26eb05f261a181189b25a34d7a13c619b020
Billypatrick <billypatrickperezchillcce@gmail.com>
2024-08-20 10:21:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+    
+}
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+
+        }
+
+    }
+    return stringEncriptada
+}
+
+
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    
+}
+
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1] , matrizCodigo[i][0] )
+        }
+    }
+    return stringDesencriptada
+}
+
+
+// script.js
+
+// Función para copiar el texto del área de mensaje y luego borrarlo
+function copiarTexto() {
+    mensaje.select();  // Selecciona el contenido del textarea 'mensaje'
+    document.execCommand("copy");  // Copia el contenido seleccionado al portapapeles
+    mensaje.value = "";  // Borra el contenido del textarea 'mensaje'  // Muestra un mensaje de confirmación
+}
+
+// Añadir un event listener al botón de copiar para que llame a la función 'copiarTexto' cuando se haga clic
+document.querySelector(".copiar").addEventListener("click", copiarTexto);
